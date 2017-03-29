a9b55d78eb74877ea70a176ee0426c5a9d16d05b
Prabhat <prabhat.singh@coditas.com>
2017-03-29 18:26:32 +0530

    login and signup page done

--- RIBApp/www/js/index.js
+++ RIBApp/www/js/index.js
@@ -1,46 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var app = angular.module("RIBApp", []);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+app.controller("loginCtrl", function($scope) {
+    $scope.login = function(){
+		if($scope.email == 'prabhatosingh80@gmail.com' && $scope.password == 'hello'){
+		    location="home.html"
+		   }
+		else{
+			alert("email or password incorrect");
+		}
+	}
+	
+});
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+app.controller("signupCtrl", function($scope){
+	
+	$scope.signup = function(){
+		
+		if($scope.enterPassword == $scope.reEnterPassword){
+			
+			location = "login.html"
+			alert("Successfull Signup");
+		}
+		else{
+			alert("Password does not match");
+		}
+	}
+	
+});
